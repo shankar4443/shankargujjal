bf9545c451f1d74d1c5cd3ddd860881e16db912d
shank <shank@SHANKAR>
2019-02-26 16:10:33 +0530

    hi this is my second script

---
+++ FrameWork/src/QSpisers/Script1.java
@@ -0,0 +1,15 @@
+package QSpisers;
+
+public class Script1 {
+	int a,b,c;
+	public void test(){
+		System.out.println("test is pass");
+	}
+
+	public static void main(String[] args) {
+		Script1 s1=new Script1();
+		s1.test();
+
+	}
+
+}
