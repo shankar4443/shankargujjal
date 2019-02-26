be39ee512baf1bd9ff260d006adc7761e1b2fab8
shank <shank@SHANKAR>
2019-02-26 16:03:42 +0530

    hi this is my first script

---
+++ FrameWork/src/QSpisers/Script.java
@@ -0,0 +1,13 @@
+package QSpisers;
+
+public class Script {
+	int a=12;
+	public void test(){
+		System.out.println("test is pass");
+	}
+	public static void main(String[] args) {
+		Script s=new Script();
+		s.test();
+	}
+
+}
